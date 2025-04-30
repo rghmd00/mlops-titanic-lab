6cefcdec2f3178ec23aebad29ba5593b391d55de
Raghad Mahmoud <rm1801@fayoum.edu.eg>
2025-04-30 11:55:35 +0000

    Lab 1: Added Hydra configuration and made pipeline configurable

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,9 +6,39 @@ from save_model import save_model
 from train_model import train_model
 
 
-def main(path: str = "./data/raw/train.csv"):
+# def main(path: str = "./data/raw/train.csv"):
+#     # Step 1: Load data
+#     train_df = load_data(path)
+
+#     # Step 2: Preprocess data
+#     train_df_Preprocessed = wrangle(train_df)
+
+#     # Step 3: Train model
+#     predictor = train_model(train_df_Preprocessed)
+
+#     # Step 4: Save model using pickle
+#     save_model(predictor, "models/predictor.pkl")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+# src/pipeline.py
+
+import hydra
+from omegaconf import DictConfig
+from data_loader import load_data
+from preprocessing import wrangle
+from save_model import save_model
+from train_model import train_model
+
+
+@hydra.main(config_path="../conf", config_name="config", version_base=None)
+def main(cfg: DictConfig):
     # Step 1: Load data
-    train_df = load_data(path)
+    train_df = load_data(cfg.path)
 
     # Step 2: Preprocess data
     train_df_Preprocessed = wrangle(train_df)
@@ -17,7 +47,7 @@ def main(path: str = "./data/raw/train.csv"):
     predictor = train_model(train_df_Preprocessed)
 
     # Step 4: Save model using pickle
-    save_model(predictor, "models/predictor.pkl")
+    save_model(predictor, cfg.model_output)
 
 
 if __name__ == "__main__":
